49c651e0b9e22b0063c73636e0bbdc3b50a0b375
CasLinden <cas_linden@msn.com>
2023-08-16 16:15:20 +0900

    using mongoDB as session store

--- app.js
+++ app.js
@@ -7,18 +7,24 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 const indexRouter = require('./routes/index');
 const dashboardRouter = require('./routes/dashboard');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const mongoDb = process.env.MONGODB_KEY;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_KEY,
+  collection: 'sessions'
+});
+
 const app = express();
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(session({ store: store, secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
